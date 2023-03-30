ed337633cb1d26ddfee97d7a10a225b913b4979e
LizzGarleb <redscorpion3108@gmail.com>
2023-03-30 10:31:58 -0400

    cards auto-generate based on db

--- cards.js
+++ cards.js
@@ -22,6 +22,6 @@ app.get('/cards', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
 });
