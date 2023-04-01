e361e7bde61c5e6b5d40134054f22a758374464c
LizzGarleb <redscorpion3108@gmail.com>
2023-04-01 17:11:05 -0400

    passport authentication added to the routing

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db_connection');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
 
-router.post('/signup', (req, res) => {
-    const { Nombre, Apellido, Email, Contrasena } = req.body;
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
+const users = []
+
+router.use(express.urlencoded({ extended: false }));
+router.use(flash())
+router.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+router.use(passport.initialize())
+router.use(passport.session())
+router.use(methodOverride('_method'))
+
+router.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs');
+});
+
+router.get('/login', checkAuthenticated, (req, res) => {
+  res.render('index.ejs')
+});
+
+router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login', 
+  failureFlash: true
+}))
+
+router.post('/signup', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.Contrasena, 10);
+    const { Nombre, Apellido, Email } = req.body;
     const query = 'INSERT INTO users (name, last_name, email, password) VALUES (?, ?, ?, ?)';
     
-    pool.query(query, [Nombre, Apellido, Email, Contrasena], (error, result) => {
+    pool.query(query, [Nombre, Apellido, Email, hashedPassword], (error, result) => {
       if (error) {
         console.error('Error inserting data:', error);
         res.status(500).send('Error saving data');
@@ -15,25 +55,28 @@ router.post('/signup', (req, res) => {
         res.status(200).send('Registration successful!');
       }
     });
+  } catch {
+    res.redirect('/signup')
+  }
 });
 
-router.post('/login', (req, res) => {
-    const { Email, Contrasena } = req.body;
-
-    const query = 'SELECT * FROM users WHERE email = ? AND password = ?';
-    
-    pool.query(query, [Email, Contrasena], (error, result) => {
-      if (error) {
-        console.error('Error querying data:', error);
-        res.status(500).send('Error retrieving data');
-      } else {
-        if (result.length === 1) {
-          res.status(200).send('Log in successful!'); // Redirect to dashboard if login is successful
-        } else {
-          res.status(401).send('Invalid email or password'); // Return an error message if login is unsuccessful
-        }
-      }
-    });
+router.delete('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/login');
 });
 
-module.exports = router;
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next()
+  }
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+      return res.redirect('/')
+  }
+  next()
+}
+
+module.exports = router
---
+++ routes/passport-config.js
@@ -0,0 +1,44 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const pool = require('../db_connection');
+
+function initialize(passport) {
+    const authenticateUser = async (email, password, done) => {
+        const query = 'SELECT * FROM users WHERE email = ?';
+
+        pool.query(query, [email], async (err, result) => {
+            if (err) {
+                return done(err)
+            }
+            if (result.length == 0) {
+                return done(null, false, {message: 'No user'})
+            }
+            try {
+                const user = result[0];
+                if (await bcrypt.compare(password, user.password)) {
+                    return done(null, user)
+                } else{
+                    return done(null, false, {message: 'Password incorrect'})
+                }
+            } catch (error) {
+                return done(error);
+            }
+        });
+    };
+
+    passport.use(new LocalStrategy ({usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        pool.query('SELECT * FROM users WHERE id = ?', [id], (error, result) => {
+            if (error) {
+                return done(error);
+            }
+            if (result.length == 0) {
+                return done(null, false, {message: 'No user'});
+            }
+            return done(null, result[0]);
+        });
+    });
+}
+
+module.exports = initialize
