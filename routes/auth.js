0318cf8a125c950636d6168888bd824e1e14d72f
LizzGarleb <redscorpion3108@gmail.com>
2023-04-02 21:22:01 -0400

    Implemented email verification for new users

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 const initializePassport = require('../passport-config')
 initializePassport(
@@ -13,6 +15,34 @@ initializePassport(
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
 )
+
+async function sendVerificationEmail(email, verificationToken) {
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'puertoricoaventuras@gmail.com',
+      pass: 'nierbvcxlciyjfew'
+    },
+  });
+
+  const mailOptions = {
+    from: 'puertoricoaventuras@gmail.com',
+    to: email,
+    subject: 'Email Verification - Aventuras PR',
+    html: `
+        <h3>Welcome to Aventuras PR!</h3>
+        <p>To verify your email address, please click the link below:</p>
+        <a href="${process.env.APP_URL}/verify-email?token=${verificationToken}">Verify Email Address</a>
+        `,
+  }
+
+  try {
+    await transport.sendMail(mailOptions);
+    console.log('Verification email sent');
+  } catch (error) {
+    console.error('Error sending verification email:', error);
+  }
+}
 
 router.use(express.urlencoded({ extended: false }));
 router.use(flash())
@@ -24,6 +54,10 @@ router.use(session({
 router.use(passport.initialize())
 router.use(passport.session())
 router.use(methodOverride('_method'))
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
 router.post('/login', (req, res) => {
   const { Email, Contrasena } = req.body;
@@ -52,28 +86,53 @@ router.post('/login', (req, res) => {
   
 });
 
+router.get('/signup', (req, res) => {
+  res.render('login');
+});
+
 router.post('/signup', async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.Contrasena, 10);
     const { Nombre, Apellido, Email } = req.body;
+    const verificationToken = crypto.randomBytes(64).toString('hex');
     const query = 'INSERT INTO users (name, last_name, email, password) VALUES (?, ?, ?, ?)';
     
-    pool.query(query, [Nombre, Apellido, Email, hashedPassword], (error, result) => {
+    pool.query(query, [Nombre, Apellido, Email, hashedPassword], async (error, result) => {
       if (error) {
         console.error('Error inserting data:', error);
         res.status(500).send('Error saving data');
       } else {
-        res.status(200).send('Registration successful!');
+        await sendVerificationEmail(Email, verificationToken);
+        res.status(200).send('Registration successful! Please check your email for verification.');
       }
     });
-  } catch {
-    res.redirect('/signup')
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).send('Error creating user');
   }
 });
 
 router.delete('/logout', (req, res) => {
     req.logOut();
     res.redirect('/login');
+});
+
+router.get('/verify-email', async (req, res) => {
+  const verificationToken = req.query.token;
+  
+  const query = 'UPDATE users SET verified = 1 WHERE verification_token = ?';
+  pool.query(query, [verificationToken], (error, result) => {
+    if (error) {
+      console.error('Error verifying email:', error);
+      res.status(500).send('Error verifying email');
+    } else {
+      if (result.affectedRows === 1) {
+        res.status(200).send('Email verification successful! You can now log in.');
+      } else {
+        res.status(400).send('Invalid verification token');
+      }
+    }
+  });
 });
 
 
