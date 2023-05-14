4ad1724769e46844366cded27de8c35ee09f8368
LizzGarleb <redscorpion3108@gmail.com>
2023-05-13 22:01:33 -0400

    route for newsletter implemented

---
+++ routes/subscribe.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../config/db_connection');
+
+router.post('/subscribe', (req, res) => {
+    const email = req.body.email
+    const query = 'INSERT INTO newsletter (email) VALUES (?)';
+
+    pool.query(query, [email], (err, result) => {
+        if (err) {
+            console.error('Error inserting data:', err);
+            res.status(500).send('Error saving data');
+        } else {
+            res.send(`
+                <script>
+                    alert("Subscribed with email: ${email}");
+                    window.location.href="/";
+                </script>
+            `);
+        }
+    });
+});
+
+module.exports = router
