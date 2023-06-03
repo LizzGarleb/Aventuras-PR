d1a3b9616c0735a10af36c544c8cf104dcbf1955
LizzGarleb <redscorpion3108@gmail.com>
2023-06-02 22:17:03 -0400

    carousel of information card working properly

--- public/js/information.js
+++ public/js/information.js
@@ -1,35 +1,23 @@
-// Load the Swiper library from the CDN
-const swiperUrl = "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js";
+$('#recipeCarousel').carousel({
+  interval: 3000,
+  wrap: true,
+  keyboard: true
+})
 
-// Create a script element to load the library
-const swiperScript = document.createElement("script");
-swiperScript.src = swiperUrl;
-swiperScript.async = true;
+$('.carousel .carousel-item').each(function () {
+  const minPerSlide = 1;
+  let next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
 
-// Attach the script element to the document's head
-document.head.appendChild(swiperScript);
+  for (let i = 0; i < minPerSlide; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
 
-// When the script has finished loading, create a new Swiper instance
-swiperScript.onload = function () {
-  const swiper = new Swiper('.swiper', {
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    loop: true,
-
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    keyboard: {
-      enabled: true,
-    },
-  });
-};
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
