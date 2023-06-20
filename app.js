c3d7121018c75fca20fe56ac87bdab37d63af49c
LizzGarleb <redscorpion3108@gmail.com>
2023-06-19 21:47:04 -0400

    now

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (provess.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
 
 const express = require('express');
 const bodyParser = require('body-parser');
